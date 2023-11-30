8b796aead70cfcf34fa6cca705dff9f1ee4f4579
JonasTengborg <jonas.tengborg@gmail.com>
2023-11-30 09:21:07 +0100

    commit

--- ner/test.py
+++ ner/test.py
@@ -4,7 +4,6 @@ import re
 
 # Load the trained models
 nlp1 = spacy.load("./output/model-best")  # load the best model
-nlp2 = spacy.load("sv_core_news_sm")
 
 file_path = "../test_text.txt"
 output_file_path_json = "output_entities.json"
@@ -14,12 +13,13 @@ tel_pattern = r'([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s[0-9]+(
 tel_pattern2 = r'[0-9]+([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s[0-9]+\s[0-9]+'
 tel_pattern3 = r'(([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?( ([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?)+)'
 address_pattern = r'[A-Za-z]+ä([A-Za-z0-9]+( [A-Za-z0-9]+)+), ([A-Za-z0-9]+( [A-Za-z0-9]+)+)'
+email_pattern = r'[A-Za-z]+@[A-Za-z]+\.[A-Za-z]+'
 
 with open(file_path, "r", encoding="utf-8") as file:
     text = file.read()
 
 # Process the text with the models
-models = [nlp1, nlp2]
+models = [nlp1]
 unique_entities = set()
 
 for model in models:
@@ -50,9 +50,8 @@ for entity in entities:
     elif entity["label"] == "ADDRESS":
         tel_entities.append({"label": "ADDRESS", "text": text})
 
-    elif entity["label"] == "EMAIL":
+    elif entity["label"] == "EMAIL" and re.search(address_pattern, text):
         tel_entities.append({"label": "EMAIL", "text": text})
-
 
 
 # Save TEL entities to JSON
@@ -62,8 +61,10 @@ with open(output_file_path_json, "w", encoding="utf-8") as output_file_json:
 # Save TEL entities to text file
 with open(output_file_path_txt, "w", encoding="utf-8") as output_file_txt:
     for tel_entity in tel_entities:
-        output_file_txt.write("{}\n".format(tel_entity["text"].replace('\n', ' ')))
+        output_file_txt.write("{}\n".format(
+            tel_entity["text"].replace('\n', ' ')))
 
 # Print TEL entities to console
 for tel_entity in tel_entities:
-    print("{} {}".format(tel_entity["label"],tel_entity["text"].replace('\n', ' ')))
+    print("{} {}".format(tel_entity["label"],
+          tel_entity["text"].replace('\n', ' ')))
