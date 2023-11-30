55f07068dac6ab438a6e4e7344e5dcfa33262e96
JonasTengborg <jonas.tengborg@gmail.com>
2023-11-30 13:11:05 +0100

    commit

--- ner/test.py
+++ ner/test.py
@@ -13,7 +13,7 @@ tel_pattern = r'([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s[0-9]+(
 tel_pattern2 = r'[0-9]+([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\s[0-9]+\s[0-9]+'
 tel_pattern3 = r'(([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?( ([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?)+)'
 address_pattern = r'[A-Za-z]+ä([A-Za-z0-9]+( [A-Za-z0-9]+)+), ([A-Za-z0-9]+( [A-Za-z0-9]+)+)'
-email_pattern = r'[A-Za-z]+@[A-Za-z]+\.[A-Za-z]+'
+email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.(com|net|org|edu|gov|mil|int|eu|aero|coop|museum|arpa|[a-z]{2,})\b'
 
 with open(file_path, "r", encoding="utf-8") as file:
     text = file.read()
@@ -31,40 +31,40 @@ for model in models:
 entities = [{"label": label, "text": text} for label, text in unique_entities]
 
 # Filter and Save TEL entities to JSON and text file
-tel_entities = []
+parsed_entities = []
 
 for entity in entities:
     text = entity["text"]
     if entity["label"] == "TEL" and re.search(tel_pattern, text):
-        tel_entities.append({"label": "TEL", "text": text})
+        parsed_entities.append({"label": "TEL", "text": text})
 
     elif entity["label"] == "TEL" and re.search(tel_pattern2, text):
-        tel_entities.append({"label": "TEL", "text": text})
+        parsed_entities.append({"label": "TEL", "text": text})
 
     elif entity["label"] == "TEL" and re.search(tel_pattern3, text):
-        tel_entities.append({"label": "TEL", "text": text})
+        parsed_entities.append({"label": "TEL", "text": text})
 
     elif entity["label"] == "TEL" and re.search(tel_pattern3, text):
-        tel_entities.append({"label": "TEL", "text": text})
+        parsed_entities.append({"label": "TEL", "text": text})
 
     elif entity["label"] == "ADDRESS":
-        tel_entities.append({"label": "ADDRESS", "text": text})
+        parsed_entities.append({"label": "ADDRESS", "text": text})
 
-    elif entity["label"] == "EMAIL" and re.search(address_pattern, text):
-        tel_entities.append({"label": "EMAIL", "text": text})
+    elif entity["label"] == "EMAIL" and re.search(email_pattern, text):
+        parsed_entities.append({"label": "EMAIL", "text": text})
 
 
 # Save TEL entities to JSON
 with open(output_file_path_json, "w", encoding="utf-8") as output_file_json:
-    json.dump(tel_entities, output_file_json, ensure_ascii=False, indent=4)
+    json.dump(parsed_entities, output_file_json, ensure_ascii=False, indent=4)
 
 # Save TEL entities to text file
 with open(output_file_path_txt, "w", encoding="utf-8") as output_file_txt:
-    for tel_entity in tel_entities:
+    for tel_entity in parsed_entities:
         output_file_txt.write("{}\n".format(
             tel_entity["text"].replace('\n', ' ')))
 
 # Print TEL entities to console
-for tel_entity in tel_entities:
+for tel_entity in parsed_entities:
     print("{} {}".format(tel_entity["label"],
           tel_entity["text"].replace('\n', ' ')))
